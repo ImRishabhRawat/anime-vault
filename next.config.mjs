d91e9e9a6f922965cb06a7445a55bb11917f46a4
ImRishabhRawat <rishabhrawat757@gmail.com>
2024-01-26 15:01:19 +0530

    added transition on hover image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["shikimori.one", "other-domain.com"],
+		// domains: ["shikimori.one", "other-domain.com"],
+		remotePatterns: [
+			{ protocol: "https", hostname: "shikimori.one", pathname: "**" },
+			{ protocol: "https", hostname: "other-domain.com", pathname: "**" },
+		],
 	},
 };
 
